c6c5b30a5612111363ecccc13f71d9cc1a9cc5df
UnstoppableMoron <cte-owen.baker@sllboces.org>
2025-01-08 18:31:22 +0000

    jascpt

---
+++ exJSVariables.js
@@ -0,0 +1,7 @@
+// Introduction to JavaScript Variables
+
+// 1. Using var (older way of declaring variables, avoid in modern code)
+var age = 16; // This is a number variable, specifically an integer
+console.log("Using var: The age is " + age);
+
+// Change the valus of a variable declared with let
